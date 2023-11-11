ab7b1e46197014667ea4ad356fb8a33faba892b2
Rifqi Mufiddin <96222412+Ricle7@users.noreply.github.com>
2023-11-11 20:38:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import streamlit.components.v1 as stc
 from ml_app import run_ml_app
 
 html_temp = """
-            <div style="background-color:#3872fb;padding:10px;border-radius:10px">
-		    <h1 style="color:white;text-align:center;">Employee Promotion Prediction App </h1>
-		    <h4 style="color:white;text-align:center;">HR Team </h4>
-		    </div>
+           <div style="background: radial-gradient(circle, #1a237e, #00bcd4); padding:10px; border-radius:10px;">
+    	   <h1 style="color:white;text-align:center;">Employee Promotion Prediction App </h1>
+    	   <h4 style="color:white;text-align:center;">HR Team </h4>
+	   </div>
             """
 
 desc_temp = """
